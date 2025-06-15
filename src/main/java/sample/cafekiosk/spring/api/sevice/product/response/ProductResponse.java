7335baa1156875c5e0a54831b4d65ffd8b390172
yeonjue-2 <ilov-@naver.com>
2025-06-15 20:11:16 +0900

    feat: - JPA 기반 Product 클래스 생성 및 한 메서드 블럭 생성 - 애플리케이션 설정

---
+++ src/main/java/sample/cafekiosk/spring/api/sevice/product/response/ProductResponse.java
@@ -0,0 +1,40 @@
+package sample.cafekiosk.spring.api.sevice.product.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+@Getter
+public class ProductResponse {
+
+    private Long id;
+    private String productNumber;
+    private ProductType type;
+    private ProductSellingStatus sellingStatus;
+    private String name;
+    private String price;
+
+    @Builder
+    private ProductResponse(Long id, String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, String price) {
+        this.id = id;
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static ProductResponse of(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .productNumber(product.getProductNumber())
+                .type(product.getType())
+                .sellingStatus(product.getSellingStatus())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+    }
+
+}
